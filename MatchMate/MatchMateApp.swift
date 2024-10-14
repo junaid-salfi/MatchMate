a5d3b137da889120482f709ad1abb7a5e3d0d2b9
junaid-salfi <junaid.nazir2144@gmail.com>
2024-10-14 12:51:06 +0530

    model structure creation completed

--- MatchMate/MatchMateApp.swift
+++ MatchMate/MatchMateApp.swift
@@ -13,8 +13,6 @@ struct MatchMateApp: App {
 
     var body: some Scene {
         WindowGroup {
-            ContentView()
-                .environment(\.managedObjectContext, persistenceController.container.viewContext)
         }
     }
 }
---
+++ MatchMate/Model/ShadiProfile.swift
@@ -0,0 +1,51 @@
+//
+//  ShadiProfile.swift
+//  MatchMate
+//
+//  Created by Test on 14/10/24.
+//
+
+import Foundation
+
+struct ShadiProfile: Decodable {
+    var gender, phone, cell, email, nat: String
+    var name: NameInfo
+    var location: LocationInfo
+    var dob: DateOfBirthInfo
+    var picture: PictureInfo
+}
+
+struct NameInfo: Decodable {
+    var title, first, last : String
+}
+
+struct LocationInfo: Decodable {
+    var city, state, country, postcode: String
+    var street: StreetInfo
+    var coordinates: CoordinateInfo
+    var timezone: TimeZoneInfo
+}
+
+struct StreetInfo: Decodable {
+    var number: Int
+    var name: String
+}
+
+struct CoordinateInfo: Decodable {
+    var latitude, longitude: String
+}
+
+struct TimeZoneInfo: Decodable {
+    var offset, description: String
+}
+
+struct DateOfBirthInfo: Decodable {
+    var date: String
+    var age: Int
+}
+
+struct PictureInfo: Decodable {
+    var large, medium, thumbnail: String
+}
+
+
